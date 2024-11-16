91578f20a7206967b0d3039e3a14a8df01e39579
MuMSuZ <mumsuz@outlook.com>
2024-11-16 22:41:17 +0300

    Update add.js

--- add.js
+++ add.js
@@ -10,10 +10,11 @@ const addWordButton = document.getElementById('add-word');
 addWordButton.addEventListener('click', () => {
   const word = wordInput.value.trim();
   const translation = translationInput.value.trim();
+  const today = new Date().toISOString().split('T')[0]; // Tarihi al (YYYY-MM-DD formatı)
 
   if (word && translation) {
     // Kelimeyi kelime haznesine ekle
-    wordBank.push({ word, translation });
+    wordBank.push({ word, translation, date: today });
 
     // LocalStorage'a kaydet
     localStorage.setItem('wordBank', JSON.stringify(wordBank));
